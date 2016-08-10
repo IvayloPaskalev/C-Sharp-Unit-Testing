511f94ef9e84cbac8e6ef5c6baf6d6552e2da79d
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-08-09 18:50:58 -0700

    Added fef tests in CommandTests

--- 04. Workshop(Cosmetics)/Solution/Cosmetics.Tests/Engine/CommandTests.cs
+++ 04. Workshop(Cosmetics)/Solution/Cosmetics.Tests/Engine/CommandTests.cs
@@ -1,14 +1,56 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace Cosmetics.Tests.Engine
+﻿namespace Cosmetics.Tests.Engine
 {
-    [TestClass]
+    using System;
+    using NUnit.Framework;
+    using Cosmetics.Engine;
+    using System.Collections.Generic;
+
+    [TestFixture]
     public class CommandTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void Parse_ShouldReturnNewCommand_WhenTheInputStringIsValid()
         {
+            // Arrange
+            string strInput = "Input";
+
+            // Act
+            var executionResult = Command.Parse(strInput);
+
+            // Assert
+            Assert.IsInstanceOf<Command>(executionResult);
         }
+
+        [Test]
+        public void Parse_ShouldSetCorrectValue_WhenInputParameterIsValid()
+        {
+            // Arrange
+            var strInput = "RemoveFromCategory ForMale Cool";
+            var expectedNameValue = "RemoveFromCategory";
+
+            // Act
+            var executionResult = Command.Parse(strInput);
+
+            // Assert
+            Assert.AreEqual(expectedNameValue, executionResult.Name);
+        }  
+        
+        [Test]
+        public void Parse_ShouldSetCorrectProperties_WhenInputIsValid()
+        {
+            // Arrange
+            var strInput = "RemoveFromCategory ForMale Cool";
+            var expectedPropertyOne = "ForMale";
+            var expectedPropertyTwo = "Cool";
+            var expectedProperties = new List<string>();
+            expectedProperties.Add(expectedPropertyOne);
+            expectedProperties.Add(expectedPropertyTwo);
+
+            // Act
+            var exexutionResult = Command.Parse(strInput);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedProperties, exexutionResult.Parameters);
+        } 
     }
 }
