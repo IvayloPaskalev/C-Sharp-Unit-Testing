daf7eaa8448aca4c5e3de7fa12ac3f880095c87f
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-11-28 14:32:12 +0200

    Some tests

---
+++ 04. Workshop(Cosmetics)/Solution/Cosmetics.Tests/Engine/CosmeticsFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Cosmetics.Tests.Engine
+{
+    using System;
+    using NUnit.Framework;
+    using Cosmetics.Engine;
+    using Cosmetics.Common;
+
+    [TestFixture]
+    public class CosmeticsFactoryTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        public void CreateShampoo_ShouldThrowNullReferenceException_WhenNameIsNullOrEmpty(string name)
+        {
+            // Arrange
+            var cosmeticsFactory = new CosmeticsFactory();
+
+            // Act and Assert
+            Assert.Throws<NullReferenceException>
+                (() => cosmeticsFactory.CreateShampoo(name, "eexample", 10.0M, GenderType.Women, 124, UsageType.EveryDay));
+        }
+    }
+}
